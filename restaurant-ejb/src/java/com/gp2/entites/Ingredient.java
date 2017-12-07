864ee6a374628700793b872375998384a213b8b1
Yous <y.massaoudi@yahoo.fr>
2017-12-07 11:06:38 +0100

    Classe Entity

---
+++ restaurant-ejb/src/java/com/gp2/entites/Ingredient.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gp2.entites;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author cdi314
+ */
+@Entity
+public class Ingredient implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nomIngredient;
+    private float apportCalorique;
+    private float quantité;
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ingredient)) {
+            return false;
+        }
+        Ingredient other = (Ingredient) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.gp2.entites.Ingredient[ id=" + id + " ]";
+    }
+    
+}
---
+++ restaurant-ejb/src/java/com/gp2/entites/NatureProduit.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gp2.entites;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author cdi314
+ */
+@Entity
+public class NatureProduit implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id   
+    private String nomNatureProduit;
+    
+
+    public String getNomNatureProduit() {
+        return nomNatureProduit;
+    }
+
+    public void setNomNatureProduit(String nomNatureProduit) {
+        this.nomNatureProduit = nomNatureProduit;
+    }
+    
+   
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (nomNatureProduit != null ? nomNatureProduit.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NatureProduit)) {
+            return false;
+        }
+        NatureProduit other = (NatureProduit) object;
+        if ((this.nomNatureProduit == null && other.nomNatureProduit != null) || (this.nomNatureProduit != null && !this.nomNatureProduit.equals(other.nomNatureProduit))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.gp2.entites.NatureProduit[ id=" + nomNatureProduit + " ]";
+    }
+    
+}
