b410f66e3587e964af2dc6475fce1f30a1833f2d
Yous <y.massaoudi@yahoo.fr>
2017-12-07 23:49:16 +0100

    Update classes Entity

--- restaurant-ejb/src/java/com/gp2/entites/Formule.java
+++ restaurant-ejb/src/java/com/gp2/entites/Formule.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.gp2.entites;
 
 import java.io.Serializable;
@@ -11,15 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- *
- * @author cdi314
- */
+
 @Entity
 public class Formule implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nomFormul;
     private float prix;
@@ -33,29 +26,29 @@ public class Formule implements Serializable {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Formule)) {
-            return false;
-        }
-        Formule other = (Formule) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
+//    @Override
+//    public int hashCode() {
+//        int hash = 0;
+//        hash += (id != null ? id.hashCode() : 0);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object object) {
+//        // TODO: Warning - this method won't work in the case the id fields are not set
+//        if (!(object instanceof Formule)) {
+//            return false;
+//        }
+//        Formule other = (Formule) object;
+//        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+//            return false;
+//        }
+//        return true;
+//    }
 
     @Override
     public String toString() {
-        return "com.gp2.entites.Formule[ id=" + id + " ]";
+        return nomFormul + " ("+ prix + " )" ;
     }
     
 }
